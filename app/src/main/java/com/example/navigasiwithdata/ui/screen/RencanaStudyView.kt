c211c07b864b81f7813b04964cc37263a544e250
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-11-22 06:45:43 +0700

    menambahkan column dengan row didalamnya

--- app/src/main/java/com/example/navigasiwithdata/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigasiwithdata/ui/screen/RencanaStudyView.kt
@@ -1,11 +1,22 @@
 package com.example.navigasiwithdata.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.navigasiwithdata.R
 import com.example.navigasiwithdata.model.Mahasiswa
 
 @Composable
@@ -22,6 +33,21 @@ fun RencanaStudyView(
     var checked by remember { mutableStateOf(false) }
     var pilihanKelas by remember { mutableStateOf("") }
 
-    var listData: MutableList<String> = mutableListOf(choseDropdown,pilihanKelas)
-
+    var listData: MutableList<String> = mutableListOf(choseDropdown, pilihanKelas)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.primary
+                )
+            )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) { }
+    }
 }
