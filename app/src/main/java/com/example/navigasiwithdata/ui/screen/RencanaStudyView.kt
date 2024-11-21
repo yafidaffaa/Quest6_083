de6fb96f1cc2bd1df8722ea9e6d13a785739c47b
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-11-22 06:52:06 +0700

    menambahkan image dengan spacer

--- app/src/main/java/com/example/navigasiwithdata/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigasiwithdata/ui/screen/RencanaStudyView.kt
@@ -1,11 +1,15 @@
 package com.example.navigasiwithdata.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,7 +18,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.navigasiwithdata.R
 import com.example.navigasiwithdata.model.Mahasiswa
@@ -48,6 +54,17 @@ fun RencanaStudyView(
                 .fillMaxWidth()
                 .padding(16.dp),
             verticalAlignment = Alignment.CenterVertically
-        ) { }
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.yafi),
+                contentDescription = null,
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+
+            )
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+
+        }
     }
 }
