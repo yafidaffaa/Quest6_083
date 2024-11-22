89847734d3be5a1aea2bac46d90759326984ada0
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-11-22 07:26:55 +0700

    menambahkan dua buah button

--- app/src/main/java/com/example/navigasiwithdata/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigasiwithdata/ui/screen/RencanaStudyView.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -188,7 +189,18 @@ fun RencanaStudyView(
                     )
                 }
                 Spacer(modifier = Modifier.padding(8.dp))
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Button(onClick = {onBackButtonClicked()}) {
+                        Text(text = "Kembali")
+                    }
+                    Button(onClick = {onSubmitButtonClicked(listData)}, enabled = checked) {
+                        Text(text = "Lanjut")
 
+                    }
+                }
             }
         }
     }
