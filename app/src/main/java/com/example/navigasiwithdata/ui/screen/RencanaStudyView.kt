da7a82b3acb71e2339e094f142f2bcff25e7148a
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-11-22 07:13:07 +0700

    menambahkan dua text dengan spacer

--- app/src/main/java/com/example/navigasiwithdata/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigasiwithdata/ui/screen/RencanaStudyView.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -130,6 +131,18 @@ fun RencanaStudyView(
                     options = MataKuliah.options,
                     label = "Mata Kuliah",
                     onValueChangeEvent = { choseDropdown = it }
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                HorizontalDivider()
+                Spacer(modifier = Modifier.padding(8.dp))
+                Text(
+                    text = "Pilih Kelas Belajar",
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Silahkan pilih kelas dari mata kuliah yang anda inginkan",
+                    fontWeight = FontWeight.Light,
+                    fontSize = 12.sp
                 )
                 Spacer(modifier = Modifier.padding(8.dp))
 
