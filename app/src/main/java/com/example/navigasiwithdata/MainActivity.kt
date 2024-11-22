e682f746e16f62716b03b17355efe8bad4d31aae
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-11-22 08:19:21 +0700

    memanggil fungsi mahasiswa app sebagai navigasi di main activity

--- app/src/main/java/com/example/navigasiwithdata/MainActivity.kt
+++ app/src/main/java/com/example/navigasiwithdata/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.navigasiwithdata.navigation.MahasiswaApp
 import com.example.navigasiwithdata.ui.theme.NavigasiwithdataTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             NavigasiwithdataTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MahasiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
