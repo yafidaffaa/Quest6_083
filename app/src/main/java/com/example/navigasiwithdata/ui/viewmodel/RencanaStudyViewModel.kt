fb4352348d1ea5a71a7dbb5e9545255b152be599
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-11-22 07:30:34 +0700

    menambahkan private variable

--- app/src/main/java/com/example/navigasiwithdata/ui/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/navigasiwithdata/ui/viewmodel/RencanaStudyViewModel.kt
@@ -1,4 +1,9 @@
 package com.example.navigasiwithdata.ui.viewmodel
 
+import com.example.navigasiwithdata.model.RencanaStudy
+import kotlinx.coroutines.flow.MutableStateFlow
+
 class RencanaStudyViewModel {
+    private val _krsState = MutableStateFlow(RencanaStudy())
+
 }
