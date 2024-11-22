040a6aa28f0cd433bb98a8cccba39dff6e508d94
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-11-22 07:31:44 +0700

    menambahkan variable

--- app/src/main/java/com/example/navigasiwithdata/ui/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/navigasiwithdata/ui/viewmodel/RencanaStudyViewModel.kt
@@ -2,8 +2,12 @@ package com.example.navigasiwithdata.ui.viewmodel
 
 import com.example.navigasiwithdata.model.RencanaStudy
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class RencanaStudyViewModel {
     private val _krsState = MutableStateFlow(RencanaStudy())
+    val krsStateUi: StateFlow<RencanaStudy> = _krsState.asStateFlow()
+
 
 }
